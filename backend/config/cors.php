3c2e2a072039de7e8f2481179564656b812969b1
samane <samane.naseri66@gmail.com>
2026-07-28 10:04:00 +0330

    added admin panel and backend section to the project

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','http://localhost:3001', 'http://localhost:5173', 'http://admin.beanhouse.ir','https://admin.beanhouse.ir','https://beanhouse.ir',,'http://beanhouse.ir'],
+    'allowed_origins' => ['http://localhost:3000','http://localhost:3001', 'http://localhost:5173', 'http://admin.beanhouse.ir','https://admin.beanhouse.ir','https://beanhouse.ir','http://beanhouse.ir'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
